b5079753a39bac0da776b75af2844e0290a3db12
harshv5904 <harshv5094@gmail.com>
2023-12-01 18:05:41 +0530

    feat: Finishing Thank You Card

--- src/pages/ThankYouCard.jsx
+++ src/pages/ThankYouCard.jsx
@@ -1,7 +1,20 @@
+import { useContext } from 'react'
+import Illustartion from '/illustration-thank-you.svg'
+import { RatingContext } from '../App'
+
 function ThankYou() {
+  const { rating } = useContext(RatingContext)
   return (
     <div className="container">
-      <h1>ThankYou Page</h1>
+      <div className="center">
+        <img src={Illustartion} alt="Illustration Thank You Icon" />
+        <section className="subline">You selected {rating} out of 5</section>
+        <section className="title">Thank you!</section>
+        <p>
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don't hesitate to get in touch!
+        </p>
+      </div>
     </div>
   )
 }
